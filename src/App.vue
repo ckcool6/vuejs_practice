c00a6cc27fe8861e81a9b70413f0d05d1d8a6692
rzdtjb <rzdtjb@gmail.com>
2023-08-04 22:56:53 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home">
+        this is app templete
+        <br>
+        <button @click="one()"> 打印1</button>
+        <button @click="two()"> 打印2</button>
+        <br>
+        {{ message }}
+        <br>
+        <demo1></demo1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import demo1 from './components/demo1.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    data() {
+        return {
+            message: "hahhahahha"
+        }
+    },
+    methods: {
+        one() {
+            console.log('1111')
+        },
+        two() {
+            console.log('222')
+            console.log(this.message)
+        }
+    },
+    components: {
+        demo1
+    }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+.home {
+    width: 300px;
+    height: 300px;
+    background: yellow;
 }
 </style>
---
+++ src/components/demo1.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="btn">
+        button
+    </div>
+</template>
+
+<script>
+export default {
+    name: "demo1"
+}
+</script>
+
+<style>
+.btn {
+    width: 200px;
+    height: 50px;
+    background: red;
+    border-radius: 10px;
+    text-align: center;
+    cursor: pointer;
+    box-shadow: 0, 3px 5px #aaa;
+}
+
+.btn :hover {
+    background: blue;
+}
+</style>
